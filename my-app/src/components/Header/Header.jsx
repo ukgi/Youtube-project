19b8bf4dd73dd6baa24ce549a2549f03b89d5d49
unknown <doosan1224@naver.com>
2023-01-12 23:39:25 +0900

    [Feat] 키워드 검색 기능 구현 next : 상세페이지 UI 구현

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import SearchIcon from "@mui/icons-material/Search";
 
-export default function Header() {
+export default function Header({ handleSearch }) {
+  const [keyword, setKeyword] = useState("");
+
+  const handleKeywordSearch = (e) => {
+    e.preventDefault();
+    handleSearch(keyword);
+  };
+
   return (
     <div className='md:w-3/4 md:flex md:justify-between md:items-center m-auto mb-4 pt-6'>
       <div className='md:flex md:items-center hidden'>
         <YouTubeIcon style={{ color: "red", fontSize: "48px" }} />
         <h1 className='text-3xl text-zinc-100'>Youtube</h1>
       </div>
-      <form className='flex justify-center'>
+      <form className='flex justify-center' onSubmit={handleKeywordSearch}>
         <input
           className=' block w-96 px-1.5 py-1.5 grow-1 outline-none bg-slate-900 text-white'
           type='text'
           placeholder='Search...'
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
         />
         <button className='bg-slate-500'>
           <SearchIcon style={{ color: "white", fontSize: "36px" }} />
--- my-app/src/components/Videos/Videos.jsx
+++ my-app/src/components/Videos/Videos.jsx
@@ -1,29 +1,42 @@
 import React, { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import Video from "../Video/Video";
+import { v4 as uuidv4 } from "uuid";
 
 export default function Videos() {
   const [videos, setVideos] = useState([]);
   useEffect(() => {
+    async function hotTrendRequest() {
+      const response = await fetch("/data/hotTrend.json", {
+        method: "GET",
+      });
+      const data = await response.json();
+      console.log(data.items);
+      handleSetVideos(data.items);
+    }
     hotTrendRequest();
   }, []);
 
+  const handleSetVideos = (items) => setVideos(items);
+
   return (
     <div className='bg-slate-800'>
-      <Header />
+      <Header handleSearch={searchForKeywordRequest} />
       <ul className='m-auto flex flex-wrap w-4/5 md:w-3/4'>
         {videos &&
-          videos.map((video) => <Video key={video.id} video={video} />)}
+          videos.map((video) => <Video key={uuidv4()} video={video} />)}
       </ul>
     </div>
   );
 
-  async function hotTrendRequest() {
-    const response = await fetch("/data/hotTrend.json", {
-      method: "GET",
-    });
+  async function searchForKeywordRequest(keyword) {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${keyword}&key=${process.env.REACT_APP_GOOGLE_YOUTUBE_API_KEY}`,
+      {
+        method: "GET",
+      }
+    );
     const data = await response.json();
-    console.log(data.items);
-    setVideos(data.items);
+    handleSetVideos(data.items);
   }
 }
